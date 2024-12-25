332f657dceeb50b939d4eac9964ffbd660e6945d
Rewan Khaled <83522163+RewanAbdulkariem@users.noreply.github.com>
2024-12-25 17:52:27 +0200

    .

--- downloader.py
+++ downloader.py
@@ -1,6 +1,3 @@
 import nltk
-
-# Download the 'punkt' tokenizer models
-nltk.download('punkt')
-# If you use stopwords or other resources, download them too
+nltk.download('punkt_tab')
 nltk.download('stopwords')
